304cf2d72bb134c7c4a0a995d0912ab9b8158116
Carlos Rivas <Cjrivas00@gmail.com>
2022-05-10 12:28:39 -0400

    cant figure out how to connect to mongodb to run program

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,8 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+dotenv.config();
 
+app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
   useFindAndModify: false,
@@ -19,6 +23,10 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
 
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
