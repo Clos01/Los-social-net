12e753b1fa1747ad8d074f6f52c2ff5975feee02
Carlos Rivas <Cjrivas00@gmail.com>
2022-05-10 11:22:36 -0400

    make it better

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -10,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
   useFindAndModify: false,
